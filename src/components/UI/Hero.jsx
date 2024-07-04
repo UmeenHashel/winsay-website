fb265581aaedb5d9db31939465d05ad9ea07a973
Umeen Rathnayake <umeenbg16@gmail.com>
2024-07-04 11:02:59 +0530

    developed Hero.jsx and added resouses

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import "../../styles/hero.css";
+import heroDarkImg from "../../assets/hero-img.png";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='hero_section'>
+        <div className="container">
+            <div className="hero_wrapper">
+                <div className="hero_content">
+                  <div>
+                    <h2>We're Creating Perfect</h2>
+                    <h2>Digital Products To</h2>
+                    <h2 className="highlight">Promote Your Brand</h2>
+                  </div>
+                  <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. At, itaque. 
+                    Cum modi temporibus facere corporis, necessitatibus amet sint laborum recusandae.
+                  </p>
+
+                  <div className="hero_btns">
+                    <button className="primary_btn">Get Started Now</button>
+                    <button className="secondary_btn">Discover More</button>
+                  </div>
+                </div>
+
+                <div className="hero_img">
+                  <img src={heroDarkImg} alt="hero-img"/>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
