40249b7c43aa6f2c6c5c877e6bc613d3a0818535
Umeen Rathnayake <umeenbg16@gmail.com>
2024-07-08 14:30:47 +0530

    Updated Team.jsx

--- src/components/UI/Team.jsx
+++ src/components/UI/Team.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import team01 from '../../assets/team-01.png'
+import '../../styles/team.css'
 
 const Team = () => {
-  return (
-    <div>Team</div>
-  )
+  return ( 
+    <section className='our_team'>
+        <div className="container">
+            <div className="team_content">
+                <h6 className="subtitle">Our Team</h6>
+                <h2>
+                    Meet with <span className="highlight">our team</span>
+                </h2>
+            </div>
+
+            <div className="team_wrapper">
+                <div className="team_item">
+                    <div className="team_img">
+                        <img src={team01} alt="team 01" />
+                    </div>
+                    <div className="team_details">
+                        <h4>Courtny Hurry</h4>
+                        <p className="description">Product Development</p>
+
+                        <div className="team_member_social">
+                            <span>
+                                < i class="ri-linkedin-line"></i>
+                            </span>
+                            <span>
+                                <i class="ri-facebook-line"></i>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+  );
 }
 
 export default Team
